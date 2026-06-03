7c0189d4e0f0118110dcd75129539028e65ebeb2
kamkywork <kamky.work@gmail.com>
2026-06-03 22:35:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-cconst CACHE_NAME = 'kamky-app-v25'; // <--- เปลี่ยนเลขตรงนี้ทุกครั้งที่อัปเดตเว็บ (v3, v4, v5...)
+cconst CACHE_NAME = 'kamky-app-v26'; // <--- เปลี่ยนเลขตรงนี้ทุกครั้งที่อัปเดตเว็บ (v3, v4, v5...)
 const urlsToCache = [
   './',
   './index.html',
